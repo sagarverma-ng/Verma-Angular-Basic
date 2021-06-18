cd86490083ed9aea3b1b8cb8947e5bc3b18144cb
sagar verma <sagar.verma19920@gmail.com>
2021-06-18 12:33:42 +0530

    Life cycle

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,18 +1,35 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, OnDestroy, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit, OnDestroy, OnChanges {
 
   @Input() compingFromParent: string;
   @Output() passToParentValue = new EventEmitter<string>();
-  constructor() { }
+
+  constructor() {
+    console.log('constructor');
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit');
   }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+
+
+
+
 
   PassValueToParent(): void {
     this.passToParentValue.emit(this.compingFromParent);
